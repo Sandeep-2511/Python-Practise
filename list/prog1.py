fd69d93200e1419814cc8c5c8f1e049baa1f7023
sandeep.potdukhe <potdukhesandeep7@gmail.com>
2019-07-05 14:18:40 +0530

    list

---
+++ list/prog1.py
@@ -0,0 +1,14 @@
+print("enter a list:")
+list=[]
+print(list)
+
+list.append(1)
+list.append(2)
+list.append(3)
+print("List after addition of elements:")
+print(list)
+
+for i in range(1, 6):
+    list.append(i)
+    print("The addition of numbers are:")
+    print(list)
---
+++ list/prog4.py
@@ -0,0 +1,8 @@
+i=6
+a=[]
+while i>0:
+    print("Enter no:")
+    num=input()
+    a.append(num)
+    i=i-1
+    print(a)
